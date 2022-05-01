61d2021b449979c0ef195d13271c34f9b261db98
na2na <49822810+na2na-p@users.noreply.github.com>
2022-05-02 00:58:03 +0900

    fix 半角スペース挿入

--- packages/client/src/components/global/header.vue
+++ packages/client/src/components/global/header.vue
@@ -25,7 +25,7 @@
 		</div>
 	</template>
 	<div class="buttons right">
-		<button class="_button button"onclick="location.reload();"><i class="fa-solid fa-arrow-rotate-right"></i></button>
+		<button class="_button button" onclick="location.reload();"><i class="fa-solid fa-arrow-rotate-right"></i></button>
 		<template v-if="info && info.actions && !narrow">
 			<template v-for="action in info.actions">
 				<MkButton v-if="action.asFullButton" class="fullButton" primary @click.stop="action.handler"><i :class="action.icon" style="margin-right: 6px;"></i>{{ action.text }}</MkButton>
