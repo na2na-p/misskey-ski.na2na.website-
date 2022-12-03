bd48b50d0f5a4f0d09607c6caea6605431f9bbf9
na2na-p <na2na@na2na.dev>
2022-12-04 00:08:00 +0900

    WIP

--- packages/backend/ormconfig.js
+++ packages/backend/ormconfig.js
@@ -1,15 +1,15 @@
 import { DataSource } from 'typeorm';
-import config from './built/config/index.js';
+import _config from './built/config/index.js';
 import { entities } from './built/db/postgre.js';
 
 export default new DataSource({
 	type: 'postgres',
-	host: config.db.host,
-	port: config.db.port,
-	username: config.db.user,
-	password: config.db.pass,
-	database: config.db.db,
-	extra: config.db.extra,
+	host: process.env.MISSKEY_DB_HOST || _config.db.host,
+	port: process.env.MISSKEY_DB_PORT ? parseInt(process.env.MISSKEY_DB_PORT) : _config.db.port,
+	username: process.env.MISSKEY_DB_USER || _config.db.user,
+	password: process.env.MISSKEY_DB_PASS || _config.db.pass,
+	database: process.env.MISSKEY_DB_NAME || _config.db.db,
+	extra: process.env.MISSKEY_DB_EXTRA ? JSON.parse(process.env.MISSKEY_DB_EXTRA) : _config.db.extra,
 	entities: entities,
 	migrations: ['migration/*.js'],
 });
--- packages/backend/src/config/load.ts
+++ packages/backend/src/config/load.ts
@@ -26,7 +26,65 @@ const path = process.env.NODE_ENV === 'test'
 export default function load() {
 	const meta = JSON.parse(fs.readFileSync(`${_dirname}/../../../../built/meta.json`, 'utf-8'));
 	const clientManifest = JSON.parse(fs.readFileSync(`${_dirname}/../../../../built/_client_dist_/manifest.json`, 'utf-8'));
-	const config = yaml.load(fs.readFileSync(path, 'utf-8')) as Source;
+	const _config = yaml.load(fs.readFileSync(path, 'utf-8')) as Source;
+
+	// TODO: 移行始めたらオプショナルなものについてはundefinedを詰めるようにする
+	const config: Source = {
+		url: process.env.MISSKEY_URL || _config.url,
+		port: process.env.MISSKEY_PORT ? parseInt(process.env.MISSKEY_PORT) : _config.port,
+		db: {
+			host: process.env.MISSKEY_DB_HOST || _config.db.host,
+			port: process.env.MISSKEY_DB_PORT ? parseInt(process.env.MISSKEY_DB_PORT) : _config.db.port,
+			db: process.env.MISSKEY_DB_NAME || _config.db.db,
+			user: process.env.MISSKEY_DB_USER || _config.db.user,
+			pass: process.env.MISSKEY_DB_PASS || _config.db.pass,
+			disableCache: Boolean(process.env.MISSKEY_DB_DISABLE_DB_CACHE) || _config.db.disableCache,
+			extra: process.env.MISSKEY_DB_EXTRA ? JSON.parse(process.env.MISSKEY_DB_EXTRA) : _config.db.extra,
+		},
+		// 使用しない場合存在させてはいけない
+		redis: {
+			host: process.env.MISSKEY_REDIS_HOST || _config.redis.host,
+			port: process.env.MISSKEY_REDIS_PORT ? parseInt(process.env.MISSKEY_REDIS_PORT) : _config.redis.port,
+			family: process.env.MISSKEY_REDIS_FAMILY ? parseInt(process.env.MISSKEY_REDIS_FAMILY) : _config.redis.family,
+			pass: process.env.MISSKEY_REDIS_PASS || _config.redis.pass,
+			prefix: process.env.MISSKEY_REDIS_PREFIX || _config.redis.prefix,
+			db: process.env.MISSKEY_REDIS_DB ? parseInt(process.env.MISSKEY_REDIS_DB) : _config.redis.db,
+		},
+		// 使用しない場合存在させてはいけない
+		elasticsearch: {
+			host: process.env.MISSKEY_ELASTICSEARCH_HOST || _config.elasticsearch.host,
+			port: process.env.MISSKEY_ELASTICSEARCH_PORT ? parseInt(process.env.MISSKEY_ELASTICSEARCH_PORT) : _config.elasticsearch.port,
+			ssl: process.env.MISSKEY_ELASTICSEARCH_SSL ? Boolean(process.env.MISSKEY_ELASTICSEARCH_SSL) : _config.elasticsearch.ssl,
+			user: process.env.MISSKEY_ELASTICSEARCH_USER || _config.elasticsearch.user,
+			pass: process.env.MISSKEY_ELASTICSEARCH_PASS || _config.elasticsearch.pass,
+			// そもそもこれ必要なの？
+			index: process.env.MISSKEY_ELASTICSEARCH_INDEX || _config.elasticsearch.index,
+		},
+		id: process.env.MISSKEY_ID || _config.id,
+		disableHsts: Boolean(process.env.MISSKEY_DISABLE_HSTS) || _config.disableHsts,
+		proxy: process.env.MISSKEY_PROXY || _config.proxy,
+		proxySmtp: process.env.MISSKEY_PROXY_SMTP || _config.proxySmtp,
+		proxyBypassHosts: process.env.MISSKEY_PROXY_BYPASS_HOSTS ? process.env.MISSKEY_PROXY_BYPASS_HOSTS.split(',') : _config.proxyBypassHosts,
+		allowedPrivateNetworks: process.env.MISSKEY_ALLOWED_PRIVATE_NETWORKS ? process.env.MISSKEY_ALLOWED_PRIVATE_NETWORKS.split(',') : _config.allowedPrivateNetworks,
+		maxFileSize: process.env.MISSKEY_MAX_FILE_SIZE ? parseInt(process.env.MISSKEY_MAX_FILE_SIZE) : _config.maxFileSize,
+		// これ必要なの？
+		accesslog: process.env.MISSKEY_ACCESSLOG || _config.accesslog,
+		clusterLimit: process.env.MISSKEY_CLUSTER_LIMIT ? parseInt(process.env.MISSKEY_CLUSTER_LIMIT) : _config.clusterLimit,
+		outgoingAddressFamily: (process.env.MISSKEY_OUTGOING_ADDRESS_FAMILY as Source['outgoingAddressFamily']) || _config.outgoingAddressFamily,
+		deliverJobConcurrency: process.env.MISSKEY_DELIVER_JOB_CONCURRENCY ? parseInt(process.env.MISSKEY_DELIVER_JOB_CONCURRENCY) : _config.deliverJobConcurrency,
+		inboxJobConcurrency: process.env.MISSKEY_INBOX_JOB_CONCURRENCY ? parseInt(process.env.MISSKEY_INBOX_JOB_CONCURRENCY) : _config.inboxJobConcurrency,
+		deliverJobPerSec: process.env.MISSKEY_DELIVER_JOB_PER_SEC ? parseInt(process.env.MISSKEY_DELIVER_JOB_PER_SEC) : _config.deliverJobPerSec,
+		inboxJobPerSec: process.env.MISSKEY_INBOX_JOB_PER_SEC ? parseInt(process.env.MISSKEY_INBOX_JOB_PER_SEC) : _config.inboxJobPerSec,
+		deliverJobMaxAttempts: process.env.MISSKEY_DELIVER_JOB_MAX_ATTEMPTS ? parseInt(process.env.MISSKEY_DELIVER_JOB_MAX_ATTEMPTS) : _config.deliverJobMaxAttempts,
+		inboxJobMaxAttempts: process.env.MISSKEY_INBOX_JOB_MAX_ATTEMPTS ? parseInt(process.env.MISSKEY_INBOX_JOB_MAX_ATTEMPTS) : _config.inboxJobMaxAttempts,
+		syslog: {
+			host: process.env.MISSKEY_SYSLOG_HOST || _config.syslog.host,
+			port: process.env.MISSKEY_SYSLOG_PORT ? parseInt(process.env.MISSKEY_SYSLOG_PORT) : _config.syslog.port,
+		},
+		mediaProxy: process.env.MISSKEY_MEDIA_PROXY || _config.mediaProxy,
+		proxyRemoteFiles: Boolean(process.env.MISSKEY_PROXY_REMOTE_FILES) || _config.proxyRemoteFiles,
+		signToActivityPubGet: Boolean(process.env.MISSKEY_SIGN_TO_ACTIVITY_PUB_GET) || _config.signToActivityPubGet,
+	};
 
 	// TODO: process.envから取得
 	const url = tryCreateUrl(config.url);
