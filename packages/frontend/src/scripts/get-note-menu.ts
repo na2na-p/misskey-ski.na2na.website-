c7046c7f36bd71e3aad88dc64370c998ad54a13a
na2na-p <na2na@na2na.dev>
2023-09-07 11:16:33 +0900

    おひとり様にリノートローカル限定継承不要なので削除

--- packages/frontend/src/scripts/get-note-menu.ts
+++ packages/frontend/src/scripts/get-note-menu.ts
@@ -570,7 +570,6 @@ export function getRenoteMenu(props: {
 				}
 
 				const configuredVisibility = defaultStore.state.rememberNoteVisibility ? defaultStore.state.visibility : defaultStore.state.defaultNoteVisibility;
-				const localOnly = defaultStore.state.rememberNoteVisibility ? defaultStore.state.localOnly : defaultStore.state.defaultNoteLocalOnly;
 
 				let visibility = appearNote.visibility;
 				visibility = smallerVisibility(visibility, configuredVisibility);
@@ -580,7 +579,6 @@ export function getRenoteMenu(props: {
 
 				if (!props.mock) {
 					misskeyApi('notes/create', {
-						localOnly,
 						visibility,
 						renoteId: appearNote.id,
 					}).then(() => {
