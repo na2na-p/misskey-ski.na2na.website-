b3d4ceac822baeefdffccd1e81f2ddef1af65baf
na2na-p <na2na@na2na.dev>
2023-12-04 14:17:33 +0900

    CW注釈必須なくす

--- packages/frontend/src/components/MkPostForm.vue
+++ packages/frontend/src/components/MkPostForm.vue
@@ -687,13 +687,13 @@ function deleteDraft() {
 }
 
 async function post(ev?: MouseEvent) {
-	if (useCw.value && (cw.value == null || cw.value.trim() === '')) {
-		os.alert({
-			type: 'error',
-			text: i18n.ts.cwNotationRequired,
-		});
-		return;
-	}
+	// if (useCw && (cw == null || cw.trim() === '')) {
+	// 	os.alert({
+	// 		type: 'error',
+	// 		text: i18n.ts.cwNotationRequired,
+	// 	});
+	// 	return;
+	// }
 
 	if (ev) {
 		const el = ev.currentTarget ?? ev.target;
